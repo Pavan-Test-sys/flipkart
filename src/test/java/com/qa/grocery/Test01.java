680190e3c5185ede38b2f7dc97daef07365a41f7
pavan <pavan@192.168.0.112>
2025-02-13 12:40:53 +0530

    i am new created class

---
+++ src/test/java/com/qa/grocery/Test01.java
@@ -0,0 +1,45 @@
+package com.qa.grocery;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Test01 {
+	
+	
+	public RemoteWebDriver driver;
+	
+	
+	@Test
+	public void method(){
+		driver=new ChromeDriver();
+		
+	}
+	
+	
+	@Test
+	public void method1() throws Exception{
+		driver.get("https://www.flipkart.com");
+		Thread.sleep(5000);
+	}
+	
+	
+	@Test
+	public void method2() throws Exception{
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+	
+		
+	}
+	
+	@Test
+	public void method3(){
+		
+		driver.findElement(By.xpath("//span[text()='Kilos']")).click();
+	}
+	
+	
+	
+
+}
